0ef8cc202dfa5eb20c1fdb150c99fe252cef5db3
mahammadmehdi <147068859+mahammadmehdi@users.noreply.github.com>
2023-11-17 19:26:24 +0400

    nem qaqa

---
+++ ogani/assets/js/script.js
@@ -0,0 +1,9 @@
+const box = document.querySelector(".box")
+const bar = document.querySelector(".bar")
+
+box.addEventListener("click", function () {   
+    bar.classList.toggle("visible")
+    
+})
+
+
